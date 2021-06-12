278bddfd5589695060885ea1ff2c2f1ab265be83
Okoth Pius <orukopius8@gmail.com>
2021-06-12 16:01:30 +0300

    feat: create model

--- crime/models.py
+++ crime/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Log(models.Model):
+    case = models.CharField(max_length=100)
+    details = models.TextField()
+    location = models.CharField(max_length=50)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.case
