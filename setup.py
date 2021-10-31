d52f7fb7270ca7bab4793e70769e4f90f41364e8
Okoth Pius <orukopius8@gmail.com>
2021-10-31 23:04:40 +0300

    feat: update

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 from setuptools import setup, find_packages
 
-setup(name='django_crime',
-      version='1.0',
-      packages=find_packages())
+setup(
+    name="django_crime",
+    description="An application used to report crime in Mandera county"
+    "for Django.",
+    version='3.2.5',
+    url="https://orukopius",
+    author="Oruko Pius",
+    author_email="test@user.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=("Django>=3",),
+)
