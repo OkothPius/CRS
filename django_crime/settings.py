c5e10c84a173a1793722f9157c50a7b7a506171d
OkothPius <orukopius8@gmail.com>
2021-11-18 22:45:55 +0300

    feat: update settings

--- django_crime/settings.py
+++ django_crime/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8a34_zu7$_f=%e2q3rltduvlkxzw6251w-2^nm%ypzjnkj$vc!'
+SECRET_KEY = '<secret>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -68,8 +68,8 @@ WSGI_APPLICATION = 'django_crime.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crime',
-        'USER': 'testoruko',
+        'NAME': 'crime_report',
+        'USER': 'oruko',
         'PASSWORD': '$pyglass',
         'HOST': '127.0.0.1',
         'PORT': '5432',
