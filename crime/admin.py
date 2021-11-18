db2b2454208daabe7ee7f1d0ccff01ce4f07a551
OkothPius <orukopius8@gmail.com>
2021-11-18 22:45:40 +0300

    feat: update admin

--- crime/admin.py
+++ crime/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Category, Log
 
+def make_status(modeladmin, request, queryset):
+    queryset.update(status='assigned')
+make_status.short_description = "Mark selected case as assigned"
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ['case', 'status']
+    ordering = ['case']
+    actions = [make_status]
+
 admin.site.register(Category)
-admin.site.register(Log)
+admin.site.register(Log, LogAdmin)
